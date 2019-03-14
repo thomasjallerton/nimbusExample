856c23f514aea799112a6cf9559721bc589782a2
Thomas Allerton <thomasjallerton@gmail.com>
2019-03-14 23:50:14 +0000

    Added FileStorageClient use

--- src/main/java/handlers/FileStorageHandlers.java
+++ src/main/java/handlers/FileStorageHandlers.java
@@ -2,29 +2,52 @@ package handlers;
 
 import annotation.annotations.deployment.AfterDeployment;
 import annotation.annotations.file.FileStorageEventType;
+import annotation.annotations.file.UsesFileStorageClient;
 import annotation.annotations.function.FileStorageServerlessFunction;
 import annotation.annotations.notification.UsesNotificationTopic;
 import clients.ClientBuilder;
+import clients.file.FileStorageClient;
 import clients.notification.NotificationClient;
 import clients.notification.Protocol;
 import wrappers.file.models.FileStorageEvent;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
 public class FileStorageHandlers {
 
     private final String FILE_UPDATE_TOPIC = "fileUpdates";
+    private final String FILE_BUCKET = "exampleBucket";
     private NotificationClient notificationClient = ClientBuilder.getNotificationClient(FILE_UPDATE_TOPIC);
+    private FileStorageClient fileStorageClient = ClientBuilder.getFileStorageClient(FILE_BUCKET);
 
-    @FileStorageServerlessFunction(bucketName = "exampleBucket", eventType = FileStorageEventType.OBJECT_CREATED)
+    @FileStorageServerlessFunction(bucketName = FILE_BUCKET, eventType = FileStorageEventType.OBJECT_CREATED)
     @UsesNotificationTopic(topic = FILE_UPDATE_TOPIC)
+    @UsesFileStorageClient(bucketName = FILE_BUCKET)
     public void newObject(FileStorageEvent event) {
         String message = "New file added: " + event.getKey() + " with size " + event.getSize() + " bytes";
+
+        String result;
+        if (event.getSize() < 10000 && event.getKey().endsWith(".txt")) {
+            InputStream file = fileStorageClient.getFile(event.getKey());
+            try (BufferedReader buffer = new BufferedReader(new InputStreamReader(file))) {
+                result = buffer.lines().collect(Collectors.joining("\n"));
+                message += " Content: " + result;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Sending message " + message);
         notificationClient.notify(message);
     }
 
-    @FileStorageServerlessFunction(bucketName = "exampleBucket", eventType = FileStorageEventType.OBJECT_DELETED)
+    @FileStorageServerlessFunction(bucketName = FILE_BUCKET, eventType = FileStorageEventType.OBJECT_DELETED)
     @UsesNotificationTopic(topic = FILE_UPDATE_TOPIC)
     public void deletedObj(FileStorageEvent event) {
-        String message = "New file added: " + event.getKey() + " with size " + event.getSize() + " bytes";
+        String message = "File deleted: " + event.getKey();
         notificationClient.notify(message);
     }
 
