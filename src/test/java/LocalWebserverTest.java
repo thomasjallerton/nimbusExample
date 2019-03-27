94bd81021cdb617e757c80d8174febd68397fd09
thomasjallerton <Hood6wipe980>
2019-03-27 22:57:42 +0000

    Can host all webservers on one port

--- src/test/java/LocalWebserverTest.java
+++ src/test/java/LocalWebserverTest.java
@@ -4,7 +4,6 @@ public class LocalWebserverTest {
 
     public static void main(String[] args) {
         LocalNimbusDeployment deployment = LocalNimbusDeployment.getNewInstance("com.allerton.nimbusExample");
-        //deployment.startWebserver("NimbusExampleWebsite", 8080);
-        deployment.startServerlessFunctionWebserver(8080);
+        deployment.startAllWebservers(8080);
     }
 }
