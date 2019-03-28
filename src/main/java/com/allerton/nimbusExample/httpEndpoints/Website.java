bbf2ffe6482001b170db23f84fb6fc53fc420707
thomasjallerton <Hood6wipe980>
2019-03-28 12:52:35 +0000

    Support for file uploads

--- src/main/java/com/allerton/nimbusExample/httpEndpoints/Website.java
+++ src/main/java/com/allerton/nimbusExample/httpEndpoints/Website.java
@@ -1,29 +1,11 @@
 package com.allerton.nimbusExample.httpEndpoints;
 
-import annotation.annotations.deployment.AfterDeployment;
+import annotation.annotations.deployment.FileUpload;
 import annotation.annotations.file.FileStorageBucket;
-import annotation.annotations.file.UsesFileStorageClient;
-import clients.ClientBuilder;
-import clients.file.FileStorageClient;
 
 @FileStorageBucket(
         bucketName = "NimbusExampleWebsite",
         staticWebsite = true
 )
-public class Website {
-
-    @AfterDeployment
-    @UsesFileStorageClient(bucketName = "NimbusExampleWebsite")
-    public String uploadFiles() {
-        FileStorageClient websiteStorage = ClientBuilder.getFileStorageClient("NimbusExampleWebsite");
-
-        websiteStorage.saveFileWithContentType("index.html",
-                "<html>\n" +
-                        "<p>Hello World!</p>\n" +
-                        "<p>This is nimbus example's webpage</p>\n" +
-                        "<p>&nbsp;</p>\n" +
-                        "</html>", "text/html");
-
-        return "Successfully saved index file";
-    }
-}
+@FileUpload(bucketName = "NimbusExampleWebsite", localPath = "src/website", targetPath = "")
+public class Website {}
